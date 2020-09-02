7f257825e0ddc654db41ed639d7b828b6c60a7e8
SongHanFeng <hanfeng.song@ecarx.com.cn>
2020-09-02 11:10:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+date_parser.py:
+基于规则的日期识别（正则表达式与jieba分词）
---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+
+import setuptools
+from setuptools import setup
+
+tests_requires = [
+    "pytest",
+    "requests",
+]
+
+install_requires = [
+    "numpy",
+    "jieba",
+]
+
+
+setup(
+    name=os.getenv("_PKG_NAME", "data_tool"),  # _PKG_NAME will be used in Makefile for dev release
+    version="0.0.1",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    url="https://github.com/shfshf/data_tool",
+    license="Apache 2.0",
+    author="Hanfeng Song",
+    author_email="1316478299@qq.com",
+    description="data_tool",
+    install_requires=install_requires,
+)
